aa4ed6a69f075af8038bb71fc3ecaa278959b5c0
Viacheslav Balashov <slavabal11@outlook.com>
2025-11-22 16:32:16 +0700

    ignore full .idea directory

---
+++ examples/thread-pools-code/src/main/java/io/github/fatalistix/AlgoTask.java
@@ -0,0 +1,4 @@
+package io.github.fatalistix;
+
+public record AlgoTask() {
+}
---
+++ examples/thread-pools-code/src/main/java/io/github/fatalistix/WriteToFileTask.java
@@ -0,0 +1,9 @@
+package io.github.fatalistix;
+
+public record IoTask() implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
